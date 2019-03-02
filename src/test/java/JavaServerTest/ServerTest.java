b995e34220b9d92688c8add0d89e5d8133230917
unknown <2128408@labinfo.is.escuelaing.edu.co>
2019-03-02 09:17:01 -0500

    Prueba

---
+++ src/test/java/JavaServerTest/ServerTest.java
@@ -0,0 +1,17 @@
+package JavaServerTest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ *
+ *
+ * @author 2128408
+ */
+public class ServerTest {
+    
+    @Test
+    public void pruebas(){
+        Assert(true);
+    }
+    
+}
